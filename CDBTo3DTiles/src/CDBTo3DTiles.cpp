1a82512087e090551aee5ac1d9a7a255971fb236
Sanjeet Suhag <suhagsanjeet@gmail.com>
2021-03-05 17:58:05 -0500

    Removes redundant GTModel workaround

--- CDBTo3DTiles/src/CDBTo3DTiles.cpp
+++ CDBTo3DTiles/src/CDBTo3DTiles.cpp
@@ -878,13 +878,10 @@ void Converter::convert()
         m_impl->flushTilesetCollection(geoCell, m_impl->hydrographyNetworkTilesets);
 
         // process GTModel
-        // TODO: Remove this workaround when EXT_mesh_gpu_instancing is implemented
-        if (true) {
-            cdb.forEachGTModelTile(geoCell, [&](CDBGTModels GTModel) {
-                m_impl->addGTModelToTilesetCollection(GTModel, GTModelDir);
-            });
-            m_impl->flushTilesetCollection(geoCell, m_impl->GTModelTilesets);
-        }
+        cdb.forEachGTModelTile(geoCell, [&](CDBGTModels GTModel) {
+            m_impl->addGTModelToTilesetCollection(GTModel, GTModelDir);
+        });
+        m_impl->flushTilesetCollection(geoCell, m_impl->GTModelTilesets);
 
         // process GSModel
         cdb.forEachGSModelTile(geoCell, [&](CDBGSModels GSModel) {
